06266592ea25ddc1bda72a2d8dcdcc540c5fe7e4
DavidCG <jookdaf@gmail.com>
2015-10-10 19:00:37 +0200

    Implement challenge 'The 3n + 1 problem'

--- ateneo/the-3n-plus-one/project.clj
+++ ateneo/the-3n-plus-one/project.clj
@@ -1,6 +1,6 @@
 (defproject the-3n-plus-one "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Ateneo :: The 3n + 1 problem"
+  :url "https://github.com/Idrek/programming-challenges"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
--- ateneo/the-3n-plus-one/src/the_3n_plus_one/core.clj
+++ ateneo/the-3n-plus-one/src/the_3n_plus_one/core.clj
@@ -1,7 +1,26 @@
 (ns the-3n-plus-one.core
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s])
   (:gen-class))
 
+(defn- cycle-length
+  "Calculate the cycle-length of a number:
+    - If number is odd, cycle with result of (3n+1)
+    - If number is even, cycle with result of (n/2)
+    - Until number gets to 1
+    - Then returns the number of steps done"
+  [number]
+  (loop [n number
+         length 0]
+    (if (= n 1)
+      (inc length)
+      (recur (if (even? n) (/ n 2) (+ (* 3 n) 1)) (inc length)))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "http://curry.ateneo.net/acm-icpc/v1/100.html"
+  ([] (-main *in*))
+  ([input-file]
+    (with-open [reader (io/reader input-file)]
+      (doseq [line (line-seq reader)]
+        (let [[number-low number-high] (map #(Integer/parseInt %) (s/split line #"\s+"))]
+          (printf "%d %d %d\n" number-low number-high (apply max (map cycle-length (range number-low number-high)))))))))
