e5f3edb30dbbbda4f8c328b7d05d547b851fe8ef
DavidCG <jookdaf@gmail.com>
2015-10-11 13:17:03 +0200

    Implement and test challenge 'Diagonal difference'

--- hacker-rank/diagonal-difference/src/diagonal_difference/core.clj
+++ hacker-rank/diagonal-difference/src/diagonal_difference/core.clj
@@ -1,7 +1,31 @@
 (ns diagonal-difference.core
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s])
+  (:use [clojure.tools.trace])
   (:gen-class))
 
+(defn get-columns-sum
+  "Matrix is a sequence of sequences
+  Columns is a sequence.
+  It return the sum of one column per row indicated by the 'columns' var"
+  [matrix columns]
+  (let [matrix-with-columns (map vector matrix columns)]
+    (reduce (fn [acum matrix-row] (+ acum (get (vec (first matrix-row)) (second matrix-row)))) 0 matrix-with-columns)))
+
+(defn get-diagonal-difference
+  "Difference between the sum of values in the diagonal from the right and the diagonal from the left"
+  ([matrix]
+    (get-diagonal-difference matrix (count matrix)))
+  ([matrix num-rows]
+    (let [diagonal-right (range num-rows)
+          diagonal-left (reverse diagonal-right)]
+      (- (get-columns-sum matrix diagonal-right) (get-columns-sum matrix diagonal-left)))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "https://www.hackerrank.com/challenges/diagonal-difference"
+  ([] (-main *in*))
+  ([input-file]
+    (with-open [reader (io/reader input-file)]
+      (let [num-rows (Integer. (first (line-seq reader)))
+            matrix (partition num-rows (map #(Integer. %) (s/split (slurp reader) #"\s+")))]
+        (println (Math/abs (get-diagonal-difference matrix num-rows)))))))
--- hacker-rank/diagonal-difference/test/diagonal_difference/core_test.clj
+++ hacker-rank/diagonal-difference/test/diagonal_difference/core_test.clj
@@ -2,6 +2,22 @@
   (:require [clojure.test :refer :all]
             [diagonal-difference.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest diagonal-difference
+
+  (testing "get-columns-sum"
+    ;; First colum
+    (is (= (+ 0 3 6) (get-columns-sum [[0 1 2] [3 4 5] [6 7 8]] [0 0 0])))
+    ;; Diagonal from left to right
+    (is (= (+ 0 4 8) (get-columns-sum [[0 1 2] [3 4 5] [6 7 8]] [0 1 2])))
+    ;; Diagonal from right to left
+    (is (= (+ 2 4 6) (get-columns-sum [[0 1 2] [3 4 5] [6 7 8]] [2 1 0]))))
+
+  (testing "get-diagonal-difference"
+    (is (= (- (+ 0 4 8) (+ 2 4 6)) (get-diagonal-difference [[0 1 2] [3 4 5] [6 7 8]])))
+    (is (= (- (+ 0 4 8) (+ 2 4 6)) (get-diagonal-difference [[0 1 2] [3 4 5] [6 7 8]] 3)))
+    (is (=
+          (- (+ 0 6 12 18 24) (+ 4 8 12 16 20))
+          (get-diagonal-difference [[0 1 2 3 4] [5 6 7 8 9] [10 11 12 13 14] [15 16 17 18 19] [20 21 22 23 24]])))
+    (is (=
+          (- (+ 0 6 12 18 24) (+ 4 8 12 16 20))
+          (get-diagonal-difference [[0 1 2 3 4] [5 6 7 8 9] [10 11 12 13 14] [15 16 17 18 19] [20 21 22 23 24]] 5)))))
