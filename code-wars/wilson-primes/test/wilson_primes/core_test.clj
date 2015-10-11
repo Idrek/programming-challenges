862a0bf245c4b52acbb0812eeb87b496d2dc7b8a
DavidCG <jookdaf@gmail.com>
2015-10-11 18:34:14 +0200

    Create new template app using Leiningen for challenge 'wilson-primes'

---
+++ code-wars/wilson-primes/test/wilson_primes/core_test.clj
@@ -0,0 +1,7 @@
+(ns wilson-primes.core-test
+  (:require [clojure.test :refer :all]
+            [wilson-primes.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
