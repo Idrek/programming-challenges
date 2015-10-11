7987cb26b6616957bbab7f292f733375a2224511
DavidCG <jookdaf@gmail.com>
2015-10-11 19:36:44 +0200

    Test challenge 'Wilson primes'

--- code-wars/wilson-primes/test/wilson_primes/core_test.clj
+++ code-wars/wilson-primes/test/wilson_primes/core_test.clj
@@ -1,7 +1,19 @@
 (ns wilson-primes.core-test
   (:require [clojure.test :refer :all]
-            [wilson-primes.core :refer :all]))
+            [wilson-primes.core :refer :all]
+            [clojure.set :as set]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest wilson-primes
+
+  (testing "factorial"
+    (is (= 1 (factorial 0)))
+    (is (= 1 (factorial 1)))
+    (is (= 2 (factorial 2)))
+    (is (= 6 (factorial 3)))
+    (is (= 24 (factorial 4))))
+
+  (testing "is-wilson-prime"
+    (are [result] (= true (is-wilson-prime result))
+      1 5 13 563)
+    (are [result] (= false (not-every? false? (map is-wilson-prime result)))
+      (set/difference (set (range 1 565)) #{1 5 13 563}))))
