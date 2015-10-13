d8986585992856d4ffe3be5b6ed5f3ceef8baad4
DavidCG <jookdaf@gmail.com>
2015-10-13 10:56:03 +0200

    Add Main method to challenge 'Unusual sort'

--- code-wars/unusual-sort/src/unusual_sort/core.clj
+++ code-wars/unusual-sort/src/unusual_sort/core.clj
@@ -1,4 +1,7 @@
 (ns unusual-sort.core
+  (:require [clojure.string :as s]
+            [clojure.java.io :as io])
+  (:use [clojure.tools.trace])
   (:gen-class))
 
 (defn char-of
@@ -6,7 +9,7 @@
   (.charAt (str character) 0))
 
 (defn unusual-sort
-  "http://www.codewars.com/kata/un-usual-sort"
+  "Sort digits after letters, including their format either as number or string"
   [& chars]
   (let [chars (flatten chars)
         sub-ascii (zipmap (map char (flatten (list (range 65 91) (range 97 123) (range 48 58)))) (range))]
@@ -15,3 +18,12 @@
          (get sub-ascii (char-of %1))
          (get sub-ascii (char-of %2)))
       chars)))
+
+(defn -main
+  "http://www.codewars.com/kata/un-usual-sort"
+  ([] (-main *in*))
+  ([input-file]
+    (with-open [reader (io/reader input-file)]
+      (doseq [entry (line-seq reader)]
+        (println "Entry: " entry)
+        (println (unusual-sort (s/split entry #"\s+")))))))
