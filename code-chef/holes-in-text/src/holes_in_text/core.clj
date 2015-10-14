3c8089c267c4c7fb70c264cb7844f0628f526fc3
DavidCG <jookdaf@gmail.com>
2015-10-14 10:57:40 +0200

    Create template app for challenge 'Holes in the text'

---
+++ code-chef/holes-in-text/src/holes_in_text/core.clj
@@ -0,0 +1,7 @@
+(ns holes-in-text.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ code-chef/holes-in-text/test/holes_in_text/core_test.clj
@@ -0,0 +1,7 @@
+(ns holes-in-text.core-test
+  (:require [clojure.test :refer :all]
+            [holes-in-text.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
