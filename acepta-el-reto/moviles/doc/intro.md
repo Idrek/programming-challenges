ea7b5c81f3b3044532c53a65d6349c93012dc88a
DavidCG <jookdaf@gmail.com>
2016-01-27 17:21:59 +0100

    Create new template app using Leiningen for challenge 'Moviles'

---
+++ acepta-el-reto/moviles/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to moviles
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ acepta-el-reto/moviles/test/moviles/core_test.clj
@@ -0,0 +1,7 @@
+(ns moviles.core-test
+  (:require [clojure.test :refer :all]
+            [moviles.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
