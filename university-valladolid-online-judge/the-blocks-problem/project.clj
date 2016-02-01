603f2c1f12eb2a331b0d412248b441276bf09219
DavidCG <jookdaf@gmail.com>
2016-02-01 22:53:43 +0100

    Move challenge 'The blocks problem' to a differente directory

--- university-valladolid-online-judge/the-blocks-problem/project.clj
+++ university-valladolid-online-judge/the-blocks-problem/project.clj
@@ -1,5 +1,5 @@
 (defproject the-blocks-problem "0.1.0-SNAPSHOT"
-  :description "Ateneo :: The blocks problem"
+  :description "UVA Online Judge :: The blocks problem :: https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=3&page=show_problem&problem=37"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
