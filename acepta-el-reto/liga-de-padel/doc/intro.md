800425ab2e99c688deccb8b1342c437739b6dacf
David CG <iddarg@gmail.com>
2016-11-16 19:52:46 +0100

    Create new challenge 'liga-de-padel'

---
+++ acepta-el-reto/liga-de-padel/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to liga-de-padel
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ acepta-el-reto/liga-de-padel/src/liga_de_padel/core.clj
@@ -0,0 +1,7 @@
+(ns liga-de-padel.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
